ed25288b5d64afb75efb57938fa9110c2975ee17
Eleonora Cocci <e.cocci@tilburguniversity.edu>
2025-10-27 16:03:55 +0100

    Initial commit

---
+++ 1_Prepare_dataset.R
@@ -0,0 +1,26 @@
+#1. MERGE VARIABLES INTO ONE DATASET ___________________________________________
+# Download packages
+library(haven)
+library(dplyr)
+library(purrr)
+
+# Set working directory 
+setwd("C:/Users/cocci/Downloads/Study material/DSS_thesis_2026/Thesis_2026_R")
+
+# List all .sav files
+sav_files <- list.files(path = ".", pattern = "\\.sav$", recursive = TRUE, full.names = TRUE)
+
+# Read each file and ensure nomem_encr exists
+read_file_safe <- function(file) {
+  df <- read_sav(file)
+  if (!"nomem_encr" %in% names(df)) {
+    stop(paste("Missing 'nomem_encr' in:", file))
+  }
+  return(df)
+}
+
+# Read and merge all files by nomem_encr
+merged_data <- sav_files %>%
+  map(read_file_safe) %>%
+  reduce(full_join, by = "nomem_encr")
+
